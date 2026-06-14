70ad21a5edd923d6d05b1353c2e4ec30be5b6be3
Polentacze <nadjathettak@gmail.com>
2026-06-14 15:48:30 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import styles from '../styles/Home.module.css';
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
@@ -7,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Made by Polentacze-Inspired by Deeeepio</title>
+        <title>Version1</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Prehistooio" />
-  <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+  <p
+          Made by Polentacze - Inspired by Deeeepio <code>pages/index.js</code>
         </p>
       </main>
 
