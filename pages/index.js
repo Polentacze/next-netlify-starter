4db80293d0b98e9d63af3197e40451a257b060af
Polentacze <nadjathettak@gmail.com>
2026-06-14 18:15:02 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Head from 'next/head'
-import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Prehistooio</title>
-      </Head>
       </Head>
 
       <main>
         <Header title="Prehistooio" />
-        <link rel="icon" type="image/png" href="https://netlify.app" />
         <p>
-          Made by Polentacze - Inspired by Deeeepio <code>pages/index.js</code>
+          Made by Polentacze - Inspired by Deeeepio.
         </p>
       </main>
 
