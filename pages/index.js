4f91f17824e01fa83dbcb9c14f75bb2ffd025cf8
Polentacze <nadjathettak@gmail.com>
2026-06-14 17:06:42 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         <Header title="Prehistooio" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="https://netlify.app" />
         <p>
           Made by Polentacze - Inspired by Deeeepio <code>pages/index.js</code>
         </p>
