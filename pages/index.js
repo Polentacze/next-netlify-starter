33d2ff5841496c667225bb15be748984a76bf84d
Polentacze <nadjathettak@gmail.com>
2026-06-14 15:58:44 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Version1</title>
+        <title>Prehistooio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
