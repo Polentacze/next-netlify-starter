1dabf6d1f8ddc3c8c58e495a89fc2a108d8154d8
Polentacze <nadjathettak@gmail.com>
2026-06-14 20:03:03 -0500

    clean build

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,4 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Prehistooio</title>
-        <link rel="icon" href="/icon.png?v=1" type="image/png" />
-      </Head>
-
-      <main>
-        <Header title="Prehistooio" />
-        <p>
-          Made by Polentacze - Inspired by Deeeepio
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+    <
