410979cef91d492e4d0969c97d4ee7679b62c218
Polentacze <nadjathettak@gmail.com>
2026-06-15 15:33:44 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,8 @@ export default function Home() {
 
       <style dangerouslySetInnerHTML={{__html: `
         @import url('https://googleapis.com');
-        .ocean-title { font-family: 'Rye', sans-serif !important; }
-        .ocean-sub { font-family: 'Rye', sans-serif !important; }
+        .ocean-title { font-family: 'Creepster', sans-serif !important; }
+        .ocean-sub { font-family: 'Creepster', sans-serif !important; }
       `}} />
 
       <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
