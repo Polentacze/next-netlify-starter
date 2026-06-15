bca57b5f1efe6e50a877b4f7956b8f2cbe2b29cf
Polentacze <nadjathettak@gmail.com>
2026-06-14 20:48:38 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import Head from 'next/head'
-import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
       <Head>
         <title>Prehistooio</title>
         <link rel="icon" href="/icon.png?v=1" type="image/png" />
       </Head>
 
-      <main>
-        <Header title="Prehistooio" />
-        <p>
+      <main style={{ marginTop: '3rem' }}>
+        <h1 style={{ fontSize: '3rem', fontWeight: '700', marginBottom: '1rem' }}>
+          Prehistooio
+        </h1>
+        <p style={{ fontSize: '1.25rem', opacity: '0.9', marginBottom: '2rem' }}>
           Made by Polentacze - Inspired by Deeeepio
         </p>
       </main>
