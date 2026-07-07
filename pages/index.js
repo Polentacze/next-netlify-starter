cf96d67e8a5dad290d60ecf2197983f819b313ca
Polentacze <nadjathettak@gmail.com>
2026-07-07 00:36:25 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -581,20 +581,15 @@ onClick={() => {
     src="/dunk-ability.png" 
     alt="Armored Guard Active" 
     style={{ position: 'absolute', top: '-65px', left: '50%', transform: 'translateX(-50%)', width: '60px', height: 'auto', backgroundColor: 'transparent', background: 'transparent', pointerEvents: 'none' }} 
-    onError={(e) => { e.target.src = "/prehistoric-skeleton.png" }} 
-  />
-)}
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <>
-        {/* Helicoprion Ability Layer (Index 3) */}
-{isAbilityActive && activeTierIndex === 3 && (
-  <img
-    src="/helicoprion-ability.png" // Your new ability asset
-    alt="Saw-Blade Strike Active"
+onError={(e) => { e.target.src = "/prehistoric-skeleton.png" }}
+      />
+    )}
+
+    {/* Helicoprion Ability Layer (Index 3) */}
+    {isAbilityActive && activeTierIndex === 3 && (
+      <img
+        src="/helicoprion-ability.png" // Your new ability asset
+        alt="Saw-Blade Strike Active"
     style={{ position: 'absolute', top: '-65px', left: '50%', transform: 'translateX(-50%)', width: '60px', height: 'auto', backgroundColor: 'transparent' }}
     onError={(e) => { e.target.src  = "/prehistoric-skeleton.png" }} 
   />
