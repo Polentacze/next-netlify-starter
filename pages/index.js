541f9dbbab2af522de03d75d665ed864246eac36
Polentacze <nadjathettak@gmail.com>
2026-07-07 16:10:58 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -594,7 +594,8 @@ onError={(e) => { e.target.src = "/prehistoric-skeleton.png" }}
     src="/helicoprion-ability.png"
     alt="Saw-Blade Strike Active"
     style={{ position: 'absolute', top: '-65px', left: '50%', transform: 'translateX(-50%)' }}
-    onError={(e) => { e.currentTarget.src = "/prehistoric-skeleton.png" }} />
+    onError={(e) => { e.currentTarget.src = "/prehistoric-skeleton.png" }}
+  />
 )}
           <img src="/trilobite.png" className="lobby-critter-one" onError={(e) => { e.target.src = "/prehistoric-skeleton.png" }} alt="critter" />
           <img src="/ammonite.png" className="lobby-critter-two" onError={(e) => { e.target.src = "/prehistoric-skeleton.png" }} alt="critter" />
