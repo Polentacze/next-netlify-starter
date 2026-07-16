cbd75d8dc9d1b97a0c4e18fb006b43a89d0a9b2e
Polentacze <nadjathettak@gmail.com>
2026-07-16 07:40:38 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -76,11 +76,10 @@ export default function Home() {
   
   const [clanInputTemp, setClanInputTemp] = useState("") 
 const evoTiers = [
-  { name: "Sacabambaspis", minScore: 0, scale: 80, file: "/sacabambaspis.png" },
-  { name: "Stethacanthus altonensis", minScore: 20, scale: 115, file: "/Stethacanthus-altonensis.png" }, //  Set to 10!
+  { name: "Sacabambaspis", minScore: 1, scale: 80, file: "/sacabambaspis.png" },
+  { name: "Stethacanthus altonensis", minScore: 10, scale: 115, file: "/Stethacanthus-altonensis.png" }, //  Set to 10!
   { name: "Dunkleosteus", minScore: 50, scale: 150, file: "/dunkleosteus.png" },                       //  Set to 50!
-  { name: "Helicoprion", minScore: 70, scale: 170, file: "/helicoprion-bes.png" }
-  { name: "Helicoprion", minScore: 100, scale: 170, file: "/Xiphiorhynchus-kimblalocki.png" }  
+  { name: "Helicoprion", minScore: 100, scale: 170, file: "/helicoprion-bes.png" }
 ]
   const [activeTierIndex, setActiveTierIndex] = useState(0)
 const [pendingEvolutionIndex, setPendingEvolutionIndex] = useState(null)
@@ -182,14 +181,12 @@ setChatMessages((p) => [...p, {
       return; //  Force exits the hook early so no evolutionary level-ups can ever process
     }
 
-if (activeTierIndex === 0 && score >= 20) {          //  Match Stethacanthus (10)
+if (activeTierIndex === 0 && score >= 10) {          //  Match Stethacanthus (10)
   if (pendingEvolutionIndex !== 1) setPendingEvolutionIndex(1)
 } else if (activeTierIndex === 1 && score >= 50) {   // 🧪 Match Dunkleosteus (50)
   if (pendingEvolutionIndex !== 2) setPendingEvolutionIndex(2)
-} else if (activeTierIndex === 2 && score >= 70) {  //  Match Helicoprion (100)
+} else if (activeTierIndex === 2 && score >= 100) {  //  Match Helicoprion (100)
   if (pendingEvolutionIndex !== 3) setPendingEvolutionIndex(3)
-} else if (activeTierIndex === 4 && score >= 100) {  //  Match xiphiorhynchus (100)
-  if (pendingEvolutionIndex !== 4) setPendingEvolutionIndex(4)
 }
   }, [score, activeTierIndex, isPlaying, username, pendingEvolutionIndex]) //  Added username monitoring to track the secret name check!
 
