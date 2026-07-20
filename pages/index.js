47682a6db61f244efd52bfadf457cc87e0a3783d
Polentacze <nadjathettak@gmail.com>
2026-07-20 17:16:48 -0500

    Update index.js test animal wiki

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ const [pendingEvolutionIndex, setPendingEvolutionIndex] = useState(null)
   const [chatMessages, setChatMessages] = useState([
     { user: "System", text: "Welcome to the Tropical sea, waters here are calm", colorCode: "#00FF1A" }
   ])
-  const slots = ["Megalodon", "Shastasaurus", "Pliosaurus", "Helicoprion", "Xiphiorhynchus", "Liopleurodon", "Stethacanthus", "Squalicorax"]
+  const slots = ["Megalodon000000", "Shastasaurus", "Pliosaurus", "Helicoprion", "Xiphiorhynchus", "Liopleurodon", "Stethacanthus", "Squalicorax"]
   const slotPositions = [{ t: "16%", l: "13.5%" }, { t: "16%", l: "24.7%" }, { t: "16%", l: "35.9%" }, { t: "16%", l: "47.1%" }, { t: "16%", l: "58.3%" }, { t: "16%", l: "69.5%" }, { t: "48%", l: "13.5%" }, { t: "48%", l: "24.7%" }]
 
 const detectTextColor = (targetString) => {
